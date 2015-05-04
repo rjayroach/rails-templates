47ad21a137e3322954bd993f06bfbfbb040d9a5a
Robert Roach <rjayroach@gmail.com>
2015-05-04 22:48:18 +0000

    add foreman, guard, rspec and liverelaod

--- template.rb
+++ template.rb
@@ -1,1 +1,32 @@
-gem 'pry-rails'
+gem 'therubyracer'
+
+gem_group :development do
+  gem 'pry-rails'
+  gem 'awesome_print'
+  gem 'rspec-rails'
+  gem 'guard-rspec', require: false
+  gem 'rack-livereload'
+  gem 'guard-livereload', '~> 2.4', require: false
+end
+
+#gsub_file 'Gemfile', /#\s*(filter_parameter_logging :password)/, '\1'
+
+def source_paths
+  Array(super) +
+    [File.join(File.expand_path(File.dirname(__FILE__)),'files')]
+end
+
+copy_file 'Guardfile'
+copy_file 'Procfile'
+
+after_bundle do
+  git :init
+  git add: '.'
+  git commit: %Q{ -m 'Initial commit' }
+  run 'bundle exec rails g rspec:install'
+  run 'rm -rf test'
+end
+
+# Remove after testing
+#generate :scaffold, 'blog', 'title:string', 'content:string'
+#rake 'db:migrate db:test:prepare'
