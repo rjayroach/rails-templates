d33c09a9127176ec7372ecb61b983ea05bf1f3fe
Robert Roach <rjayroach@gmail.com>
2015-05-11 05:44:06 +0000

    start add for docker and circle-ci

--- template.rb
+++ template.rb
@@ -1,7 +1,10 @@
 gem 'therubyracer'
-gem 'mysql2'
+
+# specificy database with -d option
+# rails new myapp -d=postgresql
 
 gem_group :development do
+  gem 'dotenv'
   gem 'pry-rails'
   gem 'awesome_print'
   gem 'rspec-rails'
@@ -10,6 +13,9 @@ gem_group :development do
   gem 'guard-livereload', '~> 2.4', require: false
   gem 'guard-ctags-bundler', require: false
 end
+
+@github_username = ask('github user name')
+@github_reponame = ask('github repo name')
 
 # See: https://github.com/rwz/ember-cli-rails
 # See also: https://libraries.io/npm/ember-cli-rails-addon
@@ -30,18 +36,26 @@ end
 
 copy_file 'Guardfile'
 copy_file 'Procfile'
+copy_file 'circle.yml'
 
 after_bundle do
   git :init
   inject_into_file '.gitignore', "\ngems.tags", after: "/tmp"
+  inject_into_file '.gitignore', "\n.env", after: "/tmp"
   git add: '.'
   git commit: %Q{ -m 'Initial commit' }
+  run "git remote add origin git@github.com:#{@github_username}/#{@github_reponame}.git"
   run 'bundle exec rails generate rspec:install'
   run 'rm -rf test'
   run 'rails generate ember-cli:init' if @ember_cli_rails
   run 'npm install --save-dev ember-cli-rails-addon@0.0.11' if @ember_cli_rails
+
+  # Remove after testing
+  generate :scaffold, 'blog', 'title:string', 'content:string'
+  rake 'db:migrate db:test:prepare'
+
 end
 
-# Remove after testing
-generate :scaffold, 'blog', 'title:string', 'content:string'
-rake 'db:migrate db:test:prepare'
+
+@include_docker = yes?('Include docker?')
+copy_file 'Dockerfile' if @include_docker
