39e426a5607dddf7c5db8273d5e31c97c7529a0f
Robert Roach <rjayroach@gmail.com>
2016-07-14 16:39:01 +0800

    template for rails 5 api mode

---
+++ template-api.rb
@@ -0,0 +1,14 @@
+
+gem 'pry-rails'
+gem 'awesome_print'
+gem 'pg'
+gem 'jsonapi-resources'
+
+gem_group :development do
+  gem 'rspec-rails'
+  gem 'factory_girl_rails'
+  gem 'dotenv-rails'
+  gem 'database_cleaner'
+end
+
+gsub_file 'Gemfile', /gem 'sqlite3'/, ''
